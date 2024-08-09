51d6d5c9ec1eab82c04ef3fe499c80adab87bd7b
developer-ccei-pool <developer@ccei-pool.com>
2024-08-09 15:59:56 +0200

    add light and flit actions + fix bug orp ph

--- core/ajax/mosquitto_pub.php
+++ core/ajax/mosquitto_pub.php
@@ -1,6 +1,6 @@
 <?php
 
-// mosquitto_pub -t 'tild_70B8F646F378/u8_w/light_code/info/desired' -h 192.168.41.38 -m '3'
+// mosquitto_pub -t 'tild_70B8F646F378/u8_w/light_state/info/desired' -h 192.168.41.38 -m '3'
 
 $device = filter_input(INPUT_POST, "device", FILTER_DEFAULT);
 $type = filter_input(INPUT_POST, "type", FILTER_DEFAULT);
@@ -12,3 +12,6 @@ $value = filter_input(INPUT_POST, "value", FILTER_DEFAULT);
 
 $output = shell_exec("mosquitto_pub -t '$device/$type/$variable/$info/$desired' -h $ip -m '$value'");
 echo "mosquitto_pub -t '$device/$type/$variable/$info/$desired' -h $ip -m '$value'";
+
+// light_state
+// light_state
